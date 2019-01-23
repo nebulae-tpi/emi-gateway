bac09d92c50ac0a14cb69d066e6b0cde03eab841
juansmolano <juansmolano@gmail.com>
2019-01-23 13:24:44 -0500

    set values for nebulae-tpi-dev

--- api/emi-gateway/test/broker/PubSubBroker.js
+++ api/emi-gateway/test/broker/PubSubBroker.js
@@ -10,7 +10,7 @@ const PubSubBroker = require('../../broker/PubSubBroker');
 let pubsubBroker = {};
 let payload = { a: 1, b: 2, c: 3 };
 
-process.env.GOOGLE_APPLICATION_CREDENTIALS = '/Users/sebastianmolano/NebulaE/Projects/TPM/emi-gateway/etc/gcloud-service-key.json';
+process.env.GOOGLE_APPLICATION_CREDENTIALS = '/Users/sebastianmolano/NebulaE/Projects/TPI/emi-gateway/etc/gcloud-service-key.json';
 
 
 describe('PUBSUB BROKER', function () {
@@ -18,7 +18,7 @@ describe('PUBSUB BROKER', function () {
         it('instance PubSubBroker', function (done) {
             pubsubBroker = new PubSubBroker({
                 replyTimeOut: 10000,
-                projectId: 'ne-tpm-prod',
+                projectId: 'nebulae-tpi-229423',
                 gatewayRepliesTopic: 'emi-gateway-replies',
                 gatewayRepliesTopicSubscription: 'emi-gateway-replies-test'
             });

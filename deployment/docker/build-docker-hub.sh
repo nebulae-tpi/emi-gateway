96e554913324790b7a3cc341d6d57da1be88e139
Felipe Santa <juan.santa@nebulae.com.co>
2019-02-09 04:23:00 -0500

    redeploy

--- deployment/docker/build-docker-hub.sh
+++ deployment/docker/build-docker-hub.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export DOCKERHUB_IMAGE=emigateway
-export DOCKERHUB_TAG=0.0.29
+export DOCKERHUB_TAG=0.0.30
 
 rm -rf deployment/docker/emi-gateway/
 cp -R $API_SHELL_PATH/emi-gateway deployment/docker/emi-gateway

3907e19d535a2598ff028234b49883189e5a12bd
Daniel Tobón Uribe <daniel.tobon@nebulae.com.co>
2022-01-25 15:27:15 -0500

    Se agrega el tipo de dato JSON a schema

--- deployment/docker/build-docker-hub.sh
+++ deployment/docker/build-docker-hub.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export DOCKERHUB_IMAGE=emigateway
-export DOCKERHUB_TAG=0.1.8
+export DOCKERHUB_TAG=0.1.9
 
 rm -rf deployment/docker/emi-gateway/
 cp -R $API_SHELL_PATH/emi-gateway deployment/docker/emi-gateway
